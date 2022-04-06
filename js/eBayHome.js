7e5d07fc5763ffe1efd94fc07df1e685a9eb900f
Quinn Lopreato <99095631+qclopreato@users.noreply.github.com>
2022-04-06 06:37:14 -0500

    Add files via upload

---
+++ js/eBayHome.js
@@ -0,0 +1,9 @@
+function myFunction(){
+    saveMoney = JSON.parse(localStorage.getItem(`saveMoney`));
+    saveEnergy = JSON.parse(localStorage.getItem(`saveEnergy`));
+    document.getElementById(`money_balance`).innerHTML = `$` + saveMoney.count;
+    document.getElementById(`money_balance`).style.color = `green`;
+    document.getElementById(`energy_balance`).innerHTML = saveEnergy.count + ` Energy`;
+    document.getElementById(`energy_balance`).style.color = `red`;
+}
+    
