4ad31ada29cc95e034a09038b893002ff69eef9a
Quinn Lopreato <99095631+qclopreato@users.noreply.github.com>
2022-04-11 06:33:11 -0500

    Update eBayHome.js

--- js/eBayHome.js
+++ js/eBayHome.js
@@ -1,8 +1,21 @@
 function balanceFunction(){
-    saveMoney = JSON.parse(localStorage.getItem(`saveMoney`));
-    saveEnergy = JSON.parse(localStorage.getItem(`saveEnergy`));
-    document.getElementById(`money_balance`).innerHTML = `$` + saveMoney.count;
-    document.getElementById(`money_balance`).style.color = `green`;
-    document.getElementById(`energy_balance`).innerHTML = saveEnergy.count + ` Energy`;
-    document.getElementById(`energy_balance`).style.color = `red`;
+    if (localStorage.getItem(`saveMoney`) && localStorage.getItem(`saveEnergy`) && localStorage.getItem(`saveTime`) && localStorage.getItem(`drinkTime`)){
+        saveMoney = JSON.parse(localStorage.getItem(`saveMoney`));
+        saveEnergy = JSON.parse(localStorage.getItem(`saveEnergy`));
+        saveTime = JSON.parse(localStorage.getItem(`saveTime`));
+        drinkTime = JSON.parse(localStorage.getItem(`drinkTime`));
+        document.getElementById(`money_balance`).innerHTML = `$` + saveMoney.count;
+        document.getElementById(`money_balance`).style.color = `green`;
+        document.getElementById(`energy_balance`).innerHTML = saveEnergy.count + ` Energy`;
+        document.getElementById(`energy_balance`).style.color = `red`;
+        document.getElementById(`time_balance`).innerHTML = Math.round(saveTime.count * 100)/100 + ` second(s)`;
+        document.getElementById(`time_balance`).style.color = `#cccc00`;
+    } else {
+        document.getElementById(`money_balance`).innerHTML = `$0`;
+        document.getElementById(`money_balance`).style.color = `green`;
+        document.getElementById(`energy_balance`).innerHTML = `0 Energy`;
+        document.getElementById(`energy_balance`).style.color = `red`;
+        document.getElementById(`time_balance`).innerHTML = `1 second(s)`
+        document.getElementById(`time_balance`).style.color = `#cccc00`;
+    }
 }
